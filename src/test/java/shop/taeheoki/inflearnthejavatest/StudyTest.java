838af73c531d4092f7770ed45272a5ac9fce5c99
taeheoki <dkqjwl2365@naver.com>
2023-09-06 20:57:18 +0900

    🧑🏻‍💻feat: JUnit 5 Assertion - 2

--- src/test/java/shop/taeheoki/inflearnthejavatest/StudyTest.java
+++ src/test/java/shop/taeheoki/inflearnthejavatest/StudyTest.java
@@ -2,8 +2,10 @@ package shop.taeheoki.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
 import java.util.function.Supplier;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -12,7 +14,7 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기 \uD83D\uDE31")
     public void create_new_study() throws Exception {
-        Study study = new Study(-10);
+        Study study = new Study(10);
         assertNotNull(study);
 //        assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다.");
 //        assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값이 DRAFT여야 한다.");
@@ -28,14 +30,14 @@ class StudyTest {
              * 이러한 부분에서 성능 개선을 가져갈 수 있다.
              */
         });
-        assertFalse(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0 보다 커야 한다.");
+        assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0 보다 커야 한다.");
 
         /**
          * 에러가 발생할 때 순차적으로 작동하게 되는데 이를 한번에 에러를 확인하는 방법이 있다.
          */
         assertAll(
                 () -> assertNotNull(study),
-                () ->  assertEquals(StudyStatus.STARTED, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다."),
+                () ->  assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다."),
                 () ->assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0 보다 커야 한다.")
         );
     }
@@ -45,6 +47,37 @@ class StudyTest {
     @DisplayName("스터디 만들기 (๓° ˘ °๓)♡")
     public void create_new_study_again() throws Exception {
         System.out.println("create1");
+    }
+
+    @Test
+    public void studyTest1() throws Exception {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        String message = exception.getMessage();
+        assertThat(message).isEqualTo("limit은 0보다 커야 한다.");
+    }
+
+    @Test
+    @DisplayName("Timeout Test")
+    public void studyTest2() throws Exception {
+        assertTimeout(Duration.ofSeconds(100), () -> {
+            new Study(10);
+            Thread.sleep(300);
+        });
+    }
+
+    @Test
+    @DisplayName("TimeoutPreemptively Test")
+    public void studyTest3() throws Exception {
+        assertTimeoutPreemptively(Duration.ofSeconds(100), () -> {
+            new Study(10);
+            Thread.sleep(300);
+        });
+        // TODO ThreadLocal
+    }
+
+    @Test
+    public void timeOutTest() throws Exception {
+        assertTimeout(Duration.ofSeconds(10), () -> new Study(10));
     }
 
     @BeforeAll
