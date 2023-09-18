1d953709e7ce90592d854e62bc43e09f4fc545a4
taeheoki <dkqjwl2365@naver.com>
2023-09-19 01:26:32 +0900

    🧑🏻‍💻feat: JUnit 5 테스트 반복하기 1부

--- src/test/java/shop/taeheoki/inflearnthejavatest/StudyTest.java
+++ src/test/java/shop/taeheoki/inflearnthejavatest/StudyTest.java
@@ -2,6 +2,8 @@ package shop.taeheoki.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 import java.util.function.Supplier;
@@ -147,6 +149,20 @@ class StudyTest {
     @DisplayName("스터디 만들기 slow")
     public void studyTest9() throws Exception {
         System.out.println("create1");
+    }
+
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("test " + repetitionInfo.getCurrentRepetition() + "/" +
+                repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
+    void parameterizedTest(String message) {
+        System.out.println(message);
     }
 
     @BeforeAll
